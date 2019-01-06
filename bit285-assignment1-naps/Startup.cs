21a6f3dd1e981f53cbded637926914bb29758630
Brian Bansenauer <brianb@cascadia.edu>
2019-01-05 19:40:04 -0800

    base-assignment

--- bit285-assignment1-naps/Startup.cs
+++ bit285-assignment1-naps/Startup.cs
@@ -25,10 +25,7 @@ namespace bit285_assignment1_naps
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseFileServer();
         }
     }
 }
